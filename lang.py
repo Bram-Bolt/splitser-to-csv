87ac2613759a3fff17a54be063fa1384a64a105b
Bram Bolt <77300464+Bram-Bolt@users.noreply.github.com>
2024-08-23 20:16:00 +0200

    Delete lang.py

--- lang.py
+++
@@ -1,9 +0,0 @@
-NL = {
-    "bal start": "Deelnemer Saldo + - + - + -",
-    "bal end": None,
-    "exp start": "Betaler Omschrijving Bedrag Datum Deelnemers",
-    "exp end": "Verder op de volgende pagina",
-}
-EN = {
-    "bal start": "Member Balance + - + - + -",
-}
