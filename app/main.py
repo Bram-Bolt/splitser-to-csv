18ae08aeb5f4ba5c0c936c71b14f93590e393c30
Bram Bolt <bram.bolt@icloud.com>
2024-08-24 12:33:48 +0200

    added comments + error handling

--- app/main.py
+++ app/main.py
@@ -1,26 +1,42 @@
-# importing required classes
 from pypdf import PdfReader
 import balance_csv
 import expenses_csv
-
-
-from lang import LANGUAGES 
 import argparse
+from lang import LANGUAGES
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 def main():
-    global language_string
-    parser = argparse.ArgumentParser(description="PDF to change to CSV")
-    parser.add_argument(
-        "filename", type=str, help="The output file name (with extension)."
-    )
-    
-    parser.add_argument('-l', '--language', type=str, default='EN', help='Language code (default: EN)')
+    try:
+        # get arguments
+        parser = argparse.ArgumentParser(description="PDF to change to CSV")
+        parser.add_argument(
+            "filename", type=str, help="The output file name (with extension)."
+        )
 
-    args = parser.parse_args()
-    language = LANGUAGES.get(args.language, LANGUAGES['EN'])
+        parser.add_argument(
+            "-l",
+            "--language",
+            type=str,
+            default="EN",
+            help="Language code (default: EN)",
+        )
 
-    balance_csv.write_csv(args.filename, language)
-    expenses_csv.write_csv(args.filename, language)
+        args = parser.parse_args()
+        # selected language
+        language = LANGUAGES.get(args.language, LANGUAGES["EN"])
+
+        # generate CSV of balances
+        balance_csv.write_csv(args.filename, language)
+
+        # Generate CSV of expenses
+        expenses_csv.write_csv(args.filename, language)
+
+        logging.info(f'Convertion of "{args.filename}" finished succesfully.')
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
 
 
 if __name__ == "__main__":
