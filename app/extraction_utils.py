e25ed2fb6d03fd5749d2ab5b59a47054643ea6f4
Bram Bolt <bram.bolt@icloud.com>
2024-08-24 12:27:57 +0200

    error handling and comments

--- app/extraction_utils.py
+++ app/extraction_utils.py
@@ -1,24 +1,43 @@
-import main
+from pypdf import PageObject
+from typing import Dict
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
+# Select rows based on start and end of table
+def select_rows(
+    page: PageObject, start_label: str, end_label: str, language: Dict[str, str]
+):
+    try:
+        # extract text
+        text_rows = page.extract_text().split("\n")
+
+        # get indicators in right language
+        start = language[start_label]
+        end = language.get(end_label)  # Use get() to avoid KeyError
+
+        # start one after heading of rows
+        start_index = text_rows.index(start) + 1
+
+        # if there is an end, select until end.
+        end_index = text_rows.index(end) if end else None
+
+        return text_rows[start_index:end_index]
+
+    except ValueError as e:
+        logging.error(f"Label not found in the text rows: {e}")
+        return []
+
+    except KeyError as e:
+        logging.error(f"Label not found in the language dictionary: {e}")
+        return []
 
 
-
-def select_rows(page, start_label, end_label, language):
-    text_rows = page.extract_text().split("\n")
-    start = language[start_label]
-    end = language[end_label]
-    if end:
-        return text_rows[text_rows.index(start) + 1 : text_rows.index(end)]
-    return text_rows[text_rows.index(start) + 1 :]
-
-
-def balances_to_float(balances):
-    if isinstance(balances, list):
-        balance_to_float = lambda balance: float(
-            balance.replace(".", "").replace(",", ".")
-        )
-        float_balances = list(map(balance_to_float, balances))
-        return float_balances
-    else:
-        return float(balances.replace(".", "").replace(",", "."))
+# Convert a balance to a float
+def amount_to_float(balance: str) -> float:
+    try:
+        return float(balance.replace(".", "").replace(",", "."))
+    except ValueError as e:
+        logging.error(f"Could not convert balance to float: {e}")
+        return None
